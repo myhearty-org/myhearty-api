f24465b332a526a9480ede4012843f9e87e24d24
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-09 23:06:13 +0800

    Add cookie and session management middleware

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
