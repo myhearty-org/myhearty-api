2b90c6faeebdab3c2768e41d8c1f75580cfeaef6
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-11 04:23:10 +0800

    Add Devise routes for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
+
+  devise_scope :user do
+    scope :users do
+      post "/sign_in", to: "devise/sessions#create"
+      post "/sign_out", to: "devise/sessions#destroy"
+
+      post "/sign_up", to: "devise/registrations#create"
+
+      post "/password", to: "devise/passwords#create"
+      patch "/password", to: "devise/passwords#update"
+
+      get "/confirmation", to: "users/confirmations#show"
+
+      post "/unlock", to: "devise/unlocks#create"
+      get "/unlock", to: "devise/unlocks#show"
+    end
+  end
 end
