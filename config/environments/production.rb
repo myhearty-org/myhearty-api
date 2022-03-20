e0ef75454241487c8ad18b2c92a8ddd20eed0ce4
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-21 05:53:13 +0800

    Change action mailer default URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => "myhearty.my" }
+  config.action_mailer.default_url_options = { :host => "api.myhearty.my" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
