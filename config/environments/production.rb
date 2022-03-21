598cb25a51706cf957a7ee27f86f4f1ba5730f23
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-21 18:59:51 +0800

    Configure Action Mailer (#7)
    * Add SendGrid API key
    
    * Configure SMTP settings for SendGrid
    
    * Change display name and email of Devise Mailer
    
    * Change action mailer default URL
    
    * Change confirmation email template
    
    * Change Devise Mailer email subjects

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,13 +57,25 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "api_production"
 
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.default_url_options = { :host => "localhost" }
+  config.action_mailer.default_url_options = { :host => "api.myhearty.my" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+    domain: "myhearty.my",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: "apikey",
+    password: Rails.application.credentials.sendgrid.api_key
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
