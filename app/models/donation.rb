2c6bed10986476a7259dac088cb5c33a80010cb7
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-28 05:03:41 +0800

    Remove unnecessary validations from `donation` model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,7 +4,5 @@ class Donation < ApplicationRecord
   belongs_to :fundraising_campaign
   belongs_to :user
 
-  validates :fundraising_campaign, presence: true
-  validates :user, presence: true
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
