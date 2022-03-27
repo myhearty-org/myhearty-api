9a92584cf20ca179717d37a2ec3b8c280244babb
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-28 04:49:48 +0800

    Remove unused attributes from `donation` model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -7,7 +7,4 @@ class Donation < ApplicationRecord
   validates :fundraising_campaign, presence: true
   validates :user, presence: true
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :payment_id, presence: true
-  validates :payment_method, presence: true
-  validates :date, presence: true
 end
