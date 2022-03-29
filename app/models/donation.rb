d6bee478bd818f6ac9d56c5161fd2bf4d03cfc34
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-30 03:57:15 +0800

    Add `donor` and `fundraising_campaign` presence validations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,5 +6,7 @@ class Donation < ApplicationRecord
 
   has_one :payment
 
+  validates :fundraising_campaign, presence: true, if: :fundraising_campaign_id_changed?
+  validates :donor, presence: true, if: :donor_id_changed?
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
