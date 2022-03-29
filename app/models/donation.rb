47bacfde50f5a09df369f91d40899e64a65c97ba
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-30 03:15:16 +0800

    Ensure donation amount is greater than 0

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,5 +6,5 @@ class Donation < ApplicationRecord
 
   has_one :payment
 
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
