fd2ac4dd5548383824183ffe7983a47d8fff15a1
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-30 04:02:50 +0800

    Change `payment` model validations
    - Add missing `allow_nil` conditions
    - Use `validate_datetime` for time validation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,10 +5,10 @@ class Payment < ApplicationRecord
   belongs_to :fundraising_campaign
   belongs_to :user
 
-  validtaes :stripe_checkout_session_id, presence: true
-  validates :gross_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :fee, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :net_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :stripe_checkout_session_id, presence: true
+  validates :gross_amount, allow_nil: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :fee, allow_nil: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :net_amount, allow_nil: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :status, presence: true
-  validates :completed_at, comparison: { greater_than_or_equal_to: :created_at }
+  validates_datetime :completed_at, allow_nil: true, after: :created_at, after_message: "must be after created at"
 end
