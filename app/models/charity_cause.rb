910bf42a8eea32b387a5cdea1b2cd8e1ec0328f5
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-30 16:52:22 +0800

    Replace `charitable_categories` with `charitables_charity_causes`

--- app/models/charity_cause.rb
+++ app/models/charity_cause.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class CharityCause < ApplicationRecord
-  has_many :charitable_categories, dependent: :delete_all
-  has_many :organizations, through: :charitable_categories, source: :charitable, source_type: "Organization"
-  has_many :fundraising_campaigns, through: :charitable_categories, source: :charitable, source_type: "FundraisingCampaign"
-  has_many :volunteer_events, through: :charitable_categories, source: :charitable, source_type: "VolunteerEvent"
-  has_many :charitable_aids, through: :charitable_categories, source: :charitable, source_type: "CharitableAid"
+  has_many :charitables_charity_causes, dependent: :delete_all
+  has_many :organizations, through: :charitables_charity_causes, source: :charitable, source_type: "Organization"
+  has_many :fundraising_campaigns, through: :charitables_charity_causes, source: :charitable, source_type: "FundraisingCampaign"
+  has_many :volunteer_events, through: :charitables_charity_causes, source: :charitable, source_type: "VolunteerEvent"
+  has_many :charitable_aids, through: :charitables_charity_causes, source: :charitable, source_type: "CharitableAid"
 end
