9091ba46455c5809bdf9e59a0ebdb96b42fa9832
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-03-30 20:45:11 +0800

    Replace `stripe_product_id` with `fundraising_campaign_id`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_104718) do
+ActiveRecord::Schema.define(version: 2022_03_30_123403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,8 @@ ActiveRecord::Schema.define(version: 2022_03_30_104718) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "organization_id", null: false
-    t.string "stripe_product_id"
+    t.string "fundraising_campaign_id", null: false
+    t.index ["fundraising_campaign_id"], name: "index_fundraising_campaigns_on_fundraising_campaign_id", unique: true
     t.index ["name"], name: "index_fundraising_campaigns_on_name"
     t.index ["organization_id"], name: "index_fundraising_campaigns_on_organization_id"
   end
