69e90faafc35dbb94da48c6986ded748913b36e3
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-01 01:27:30 +0800

    Add CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@
 
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :exception, prepend: true
 
   respond_to :json
 end
