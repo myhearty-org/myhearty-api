4e423aadd00fe1ce7df0cc2ff4c04c1146a1c012
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-01 03:45:50 +0800

    Intialize `user` instance variable inside controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,5 +3,11 @@
 module Users
   class SessionsController < Devise::SessionsController
     skip_forgery_protection only: %i[create]
+
+    def create
+      super do |user|
+        @user = user
+      end
+    end
   end
 end
