9b7127f2fef14d6391e81793715fad784e60db06
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-01 01:28:32 +0800

    Set CSRF token inside the cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,20 @@
 
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  include ActionController::Cookies
   include ActionController::RequestForgeryProtection
 
   protect_from_forgery with: :exception, prepend: true
 
+  after_action :set_csrf_cookie
+
   respond_to :json
+
+  protected
+
+  def set_csrf_cookie
+    return cookies.delete("X-CSRF-Token") unless current_user_or_member
+
+    cookies["X-CSRF-Token"] = form_authenticity_token
+  end
 end
