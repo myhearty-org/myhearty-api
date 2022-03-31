136ac1cf1765e66434241affbfd4ae23fe98748a
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-01 03:49:49 +0800

    Intialize `member` instance variable inside controllers

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -3,5 +3,11 @@
 module Members
   class SessionsController < Devise::SessionsController
     skip_forgery_protection only: %i[create]
+
+    def create
+      super do |member|
+        @member = member
+      end
+    end
   end
 end
