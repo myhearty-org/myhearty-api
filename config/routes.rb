62a65c81e59a8d968cdf0b5355c53542fae34394
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-03 05:12:44 +0800

    Move `defaults format: :json` block to the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,47 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # for unknown reason, omniauth_callbacks_controller must be declared
-  # in devise_for block, otherwise passthru method will not work
-  devise_for :users,
-             only: :omniauth_callbacks,
-             controllers: { omniauth_callbacks: "users/omniauth_callbacks" },
-             defaults: { format: :json }
+  defaults format: :json do
+    # for unknown reason, omniauth_callbacks_controller must be declared
+    # in devise_for block, otherwise passthru method will not work
+    devise_for :users,
+               only: :omniauth_callbacks,
+               controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  devise_scope :user do
-    scope :users, as: :user, defaults: { format: :json } do
-      post "/sign_in", to: "users/sessions#create"
-      post "/sign_out", to: "users/sessions#destroy"
+    devise_scope :user do
+      scope :users, as: :user do
+        post "/sign_in", to: "users/sessions#create"
+        post "/sign_out", to: "users/sessions#destroy"
 
-      post "/sign_up", to: "users/registrations#create"
+        post "/sign_up", to: "users/registrations#create"
 
-      post "/password", to: "users/passwords#create"
-      patch "/password", to: "users/passwords#update"
+        post "/password", to: "users/passwords#create"
+        patch "/password", to: "users/passwords#update"
 
-      get "/confirmation", to: "users/confirmations#show"
+        get "/confirmation", to: "users/confirmations#show"
 
-      post "/unlock", to: "users/unlocks#create"
-      get "/unlock", to: "users/unlocks#show"
+        post "/unlock", to: "users/unlocks#create"
+        get "/unlock", to: "users/unlocks#show"
+      end
     end
-  end
 
-  devise_for :members, skip: :all
+    devise_for :members, skip: :all
 
-  devise_scope :member do
-    scope :members, as: :member, defaults: { format: :json } do
-      post "/sign_in", to: "members/sessions#create"
-      post "/sign_out", to: "members/sessions#destroy"
+    devise_scope :member do
+      scope :members, as: :member do
+        post "/sign_in", to: "members/sessions#create"
+        post "/sign_out", to: "members/sessions#destroy"
 
-      post "/sign_up", to: "members/registrations#create"
+        post "/sign_up", to: "members/registrations#create"
 
-      post "/password", to: "members/passwords#create"
-      patch "/password", to: "members/passwords#update"
+        post "/password", to: "members/passwords#create"
+        patch "/password", to: "members/passwords#update"
 
-      get "/confirmation", to: "members/confirmations#show"
+        get "/confirmation", to: "members/confirmations#show"
 
-      post "/unlock", to: "members/unlocks#create"
-      get "/unlock", to: "members/unlocks#show"
+        post "/unlock", to: "members/unlocks#create"
+        get "/unlock", to: "members/unlocks#show"
+      end
     end
   end
 end
