b197f789ee2392fd1d04a2831130eafe57b223aa
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-04 03:02:06 +0800

    Add `create` action

--- app/controllers/api/v0/fundraising_campaigns_controller.rb
+++ app/controllers/api/v0/fundraising_campaigns_controller.rb
@@ -15,6 +15,40 @@ module Api
       def show
         @fundraising_campaign = FundraisingCampaign.find(params[:id])
       end
+
+      def create
+        @organization = Organization.find(params[:organization_id])
+        result = FundraisingCampaigns::CreateService.call(current_member, @organization, fundraising_campaign_params)
+        @fundraising_campaign = result.record
+
+        if result.success?
+          render :show, status: :created
+        elsif @fundraising_campaign&.errors&.any?
+          error_invalid_params(@fundraising_campaign)
+        else
+          render_error_response(message: result.message, http_status: result.http_status)
+        end
+      end
+
+      private
+
+      def fundraising_campaign_params
+        params.require(:fundraising_campaign).permit(fundraising_campaign_params_attributes)
+      end
+
+      def fundraising_campaign_params_attributes
+        %i[
+          name
+          target_amount
+          location
+          about_campaign
+          main_image
+          youtube_url
+          start_datetime
+          end_datetime
+          published
+        ]
+      end
     end
   end
 end
