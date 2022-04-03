122b6361808cba25311dc0efc760c9ff512c2464
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-04 04:58:40 +0800

    Add `create` action

--- app/controllers/api/v0/volunteer_events_controller.rb
+++ app/controllers/api/v0/volunteer_events_controller.rb
@@ -15,6 +15,41 @@ module Api
       def show
         @volunteer_event = VolunteerEvent.find(params[:id])
       end
+
+      def create
+        @organization = Organization.find(params[:organization_id])
+        result = VolunteerEvents::CreateService.call(current_member, @organization, volunteer_event_params)
+        @volunteer_event = result.record
+
+        if result.success?
+          render :show, status: :created
+        elsif @volunteer_event&.errors&.any?
+          error_invalid_params(@volunteer_event)
+        else
+          render_error_response(message: result.message, http_status: result.http_status)
+        end
+      end
+
+      private
+
+      def volunteer_event_params
+        params.require(:volunteer_event).permit(volunteer_event_params_attributes)
+      end
+
+      def volunteer_event_params_attributes
+        %i[
+          name
+          openings
+          location
+          about_event
+          main_image
+          youtube_url
+          start_datetime
+          end_datetime
+          sign_up_deadline
+          published
+        ]
+      end
     end
   end
 end
