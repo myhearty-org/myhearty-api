5b6637089c8b8a079b8fa287998aada35e2d3717
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-03 18:19:52 +0800

    Add `show` action

--- app/controllers/api/v0/organizations_controller.rb
+++ app/controllers/api/v0/organizations_controller.rb
@@ -3,10 +3,14 @@
 module Api
   module V0
     class OrganizationsController < ApiController
-      skip_forgery_protection only: %i[index]
+      skip_forgery_protection only: %i[index show]
 
       def index
         @organizations = Organization.all
+      end
+
+      def show
+        @organization = Organization.find(params[:id])
       end
     end
   end
