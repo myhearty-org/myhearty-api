54853699ab2acea8e02d5608114f0164fb1913cf
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-03 18:29:17 +0800

    Add controller test template

---
+++ test/controllers/api/v0/organizations_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+module Api
+  module V0
+    class OrganizationsControllerTest < ActionDispatch::IntegrationTest
+      # test "the truth" do
+      #   assert true
+      # end
+    end
+  end
+end
