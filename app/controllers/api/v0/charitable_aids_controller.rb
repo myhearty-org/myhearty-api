5b9f08913c9af37c0334cdad0f2a8848870ad219
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-04 09:48:20 +0800

    Add `index` action

---
+++ app/controllers/api/v0/charitable_aids_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Api
+  module V0
+    class CharitableAidsController < ApiController
+      def index
+        if params.key?(:organization_id)
+          organization = Organization.find(params[:organization_id])
+          @charitable_aids = organization.charitable_aids
+        else
+          @charitable_aids = CharitableAid.all
+        end
+      end
+    end
+  end
+end
