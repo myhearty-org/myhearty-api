182dace3d6c7e07342b1db400dfe0768c9ec5aaa
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-05 02:56:27 +0800

    Add `show` action

--- app/controllers/api/v0/volunteer_applications_controller.rb
+++ app/controllers/api/v0/volunteer_applications_controller.rb
@@ -7,6 +7,10 @@ module Api
         volunteer_event = VolunteerEvent.find(params[:volunteer_event_id])
         @volunteer_applications = volunteer_event.volunteer_applications
       end
+
+      def show
+        @volunteer_application = VolunteerApplication.find(params[:id])
+      end
     end
   end
 end
