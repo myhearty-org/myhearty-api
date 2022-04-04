f6b679c661003fd1ef494d1ee7df9fa5d093f08c
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-04 09:50:25 +0800

    Add `charitable_aids` create service

---
+++ app/services/charitable_aids/create_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module CharitableAids
+  class CreateService < BaseService
+    def initialize(member, organization, params)
+      @member = member
+      @organization = organization
+      @params = params
+    end
+
+    def call
+      return error_no_permissions unless organization_member?
+
+      charitable_aid = organization.charitable_aids.new(params)
+      charitable_aid.save ? success(record: charitable_aid) : error(record: charitable_aid)
+    end
+
+    private
+
+    attr_reader :member, :organization, :params
+
+    def organization_member?
+      organization.members.include?(member)
+    end
+
+    def error_no_permissions
+      error(
+        message: "No permission to create charitable aid",
+        http_status: :unauthorized
+      )
+    end
+  end
+end
