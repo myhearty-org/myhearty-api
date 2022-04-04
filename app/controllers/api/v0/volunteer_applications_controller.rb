649e02553d93adf7816dc58c4cecd86ed6da4afc
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-05 02:56:00 +0800

    Add `index` action

---
+++ app/controllers/api/v0/volunteer_applications_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V0
+    class VolunteerApplicationsController < ApiController
+      def index
+        volunteer_event = VolunteerEvent.find(params[:volunteer_event_id])
+        @volunteer_applications = volunteer_event.volunteer_applications
+      end
+    end
+  end
+end
