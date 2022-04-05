994744b90c6974ad417b8363a726ae398de7514b
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 02:08:06 +0800

    Authenticate `user` and `member` in `before_action`

--- app/controllers/api/v0/charitable_aid_applications_controller.rb
+++ app/controllers/api/v0/charitable_aid_applications_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V0
     class CharitableAidApplicationsController < ApiController
+      before_action :authenticate_user_or_member!, only: %i[show]
+      before_action :authenticate_member!, only: %i[index update]
+
       def index
         @charitable_aid = CharitableAid.find(params[:charitable_aid_id])
 
