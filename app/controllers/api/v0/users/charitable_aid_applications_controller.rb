1b9d2622bdb0d4304a22a8decadcf7505acbf878
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 02:30:23 +0800

    Add `apply` action

--- app/controllers/api/v0/users/charitable_aid_applications_controller.rb
+++ app/controllers/api/v0/users/charitable_aid_applications_controller.rb
@@ -14,6 +14,19 @@ module Api
 
           charitable_aid_application ? head(:no_content) : head(:not_found)
         end
+
+        def apply
+          charitable_aid = CharitableAid.find(params[:charitable_aid_id])
+          result = CharitableAidApplications::CreateService.call(current_user, charitable_aid)
+          @charitable_aid_application = result.record
+
+          case result.http_status
+          when :created
+            render :show, status: :created
+          when :not_modified
+            head :not_modified
+          end
+        end
       end
     end
   end
