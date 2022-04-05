1214810af5d5f6536503e5aae1ed47d1adacb412
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 02:29:01 +0800

    Add `applied` action

--- app/controllers/api/v0/users/charitable_aid_applications_controller.rb
+++ app/controllers/api/v0/users/charitable_aid_applications_controller.rb
@@ -7,6 +7,13 @@ module Api
         def index
           @charitable_aid_applications = current_user.charitable_aid_applications
         end
+
+        def applied
+          charitable_aid_id = params[:charitable_aid_id]
+          charitable_aid_application = CharitableAidApplication.find_by(receiver: current_user, charitable_aid_id: charitable_aid_id)
+
+          charitable_aid_application ? head(:no_content) : head(:not_found)
+        end
       end
     end
   end
