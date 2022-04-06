0c0163b436dae579e23af4f2e98ee66b6a7c8d44
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 16:25:23 +0800

    Add `find_charitable_aid_application` method

--- app/services/charitable_aid_applications/create_service.rb
+++ app/services/charitable_aid_applications/create_service.rb
@@ -8,9 +8,7 @@ module CharitableAidApplications
     end
 
     def call
-      charitable_aid_application = receiver.charitable_aid_applications
-                                           .where(charitable_aid: charitable_aid)
-                                           .first_or_initialize
+      charitable_aid_application = find_charitable_aid_application
 
       return success(record: charitable_aid_application, http_status: :not_modified) unless charitable_aid_application.new_record?
 
@@ -21,5 +19,11 @@ module CharitableAidApplications
     private
 
     attr_reader :receiver, :charitable_aid
+
+    def find_charitable_aid_application
+      receiver.charitable_aid_applications
+              .where(charitable_aid: charitable_aid)
+              .first_or_initialize
+    end
   end
 end
