35a7143060e50e32a1bb4e899f8de076a0a7ac3d
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 16:06:53 +0800

    Disallow users to cancel volunteer application if it is being processed

--- app/services/volunteer_applications/destroy_service.rb
+++ app/services/volunteer_applications/destroy_service.rb
@@ -8,9 +8,11 @@ module VolunteerApplications
     end
 
     def call
-      volunteer_application = VolunteerApplication.find_by(volunteer: volunteer, volunteer_event: volunteer_event)
+      @volunteer_application = VolunteerApplication.find_by(volunteer: volunteer, volunteer_event: volunteer_event)
 
-      return error unless volunteer_application
+      return error_not_found unless volunteer_application
+
+      return error_application_processed if processing_application?
 
       volunteer_application.delete
       success
@@ -18,6 +20,29 @@ module VolunteerApplications
 
     private
 
-    attr_reader :volunteer, :volunteer_event
+    attr_reader :volunteer, :volunteer_event, :volunteer_application
+
+    def processing_application?
+      application_processed? || deadline_exceeded?
+    end
+
+    def application_processed?
+      !volunteer_application.pending?
+    end
+
+    def deadline_exceeded?
+      Time.current > volunteer_event.application_deadline
+    end
+
+    def error_not_found
+      error(http_status: :not_found)
+    end
+
+    def error_application_processed
+      error(
+        message: "Application processed",
+        http_status: :unprocessable_entity
+      )
+    end
   end
 end
