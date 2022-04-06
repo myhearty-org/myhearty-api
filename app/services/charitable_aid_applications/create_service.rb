b380f2d1e3773b1fba80c9af3034eaabd3e9d11f
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 16:29:39 +0800

    Make `chartiable_aid_application` an instance variable

--- app/services/charitable_aid_applications/create_service.rb
+++ app/services/charitable_aid_applications/create_service.rb
@@ -8,7 +8,7 @@ module CharitableAidApplications
     end
 
     def call
-      charitable_aid_application = find_charitable_aid_application
+      @charitable_aid_application = find_charitable_aid_application
 
       return error_already_exists unless charitable_aid_application.new_record?
 
@@ -18,7 +18,7 @@ module CharitableAidApplications
 
     private
 
-    attr_reader :receiver, :charitable_aid
+    attr_reader :receiver, :charitable_aid, :charitable_aid_application
 
     def find_charitable_aid_application
       receiver.charitable_aid_applications
