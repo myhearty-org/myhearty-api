6f9bb583077f2e1e5293f25ebb9e0da4116afb31
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 19:09:50 +0800

    Add condition when the `volunteer_event` has enough volunteers

--- app/services/volunteer_applications/update_service.rb
+++ app/services/volunteer_applications/update_service.rb
@@ -11,6 +11,8 @@ module VolunteerApplications
     def call
       return error_no_permissions unless organization_member?
 
+      return error_enough_volunteers if enough_volunteers?
+
       volunteer_application.update(params) ? success : error
     end
 
@@ -22,10 +24,29 @@ module VolunteerApplications
       volunteer_application.organization.members.include?(member)
     end
 
+    def enough_volunteers?
+      params[:status] == "confirmed" && volunteer_count_exceeded?
+    end
+
+    def volunteer_count_exceeded?
+      volunteer_event.volunteer_count >= volunteer_event.openings
+    end
+
+    def volunteer_event
+      @volunteer_event ||= volunteer_application.volunteer_event
+    end
+
     def error_no_permissions
       error(
         message: "No permission to update volunteer application",
         http_status: :unauthorized
+      )
+    end
+
+    def error_enough_volunteers
+      error(
+        message: "Enough volunteers",
+        http_status: :unprocessable_entity
       )
     end
   end
