d0c9f24af5ff21b40ba8ed2e81b776cf7e2660f7
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-06 16:44:06 +0800

    Add `error_not_found` method

--- app/services/charitable_aid_applications/destroy_service.rb
+++ app/services/charitable_aid_applications/destroy_service.rb
@@ -10,7 +10,7 @@ module CharitableAidApplications
     def call
       @charitable_aid_application = CharitableAidApplication.find_by(receiver: receiver, charitable_aid: charitable_aid)
 
-      return error unless charitable_aid_application
+      return error_not_found unless charitable_aid_application
 
       charitable_aid_application.delete
       success
@@ -19,5 +19,9 @@ module CharitableAidApplications
     private
 
     attr_reader :receiver, :charitable_aid, :charitable_aid_application
+
+    def error_not_found
+      error(http_status: :not_found)
+    end
   end
 end
