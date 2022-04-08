04ded1954db20086471ea4941ec7a699f442762e
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-08 22:00:27 +0800

    Add `show` action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,4 +4,14 @@ class MembersController < ApplicationController
   def index
     @members = current_member.organization.members
   end
+
+  def show
+    @member = Member.find_by(id: params[:id], organization: current_member.organization)
+
+    if @member
+      render :show, status: :ok
+    else
+      head :not_found
+    end
+  end
 end
