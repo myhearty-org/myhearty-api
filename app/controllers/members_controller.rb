5fdd80700d682665a0f153da2c491de0a18f6f33
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-08 22:00:45 +0800

    Add `create` action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -14,4 +14,28 @@ class MembersController < ApplicationController
       head :not_found
     end
   end
+
+  def create
+    organization = current_member.organization
+    @member = Members::CreateService.call(member_params, organization)
+
+    if @member.persisted?
+      render :show, status: :ok
+    else
+      error_invalid_params(@member)
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(member_params_attributes)
+  end
+
+  def member_params_attributes
+    %i[
+      email
+      password
+    ]
+  end
 end
