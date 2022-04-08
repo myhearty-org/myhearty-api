99a951f736dc4ca5f3161fb4665584e5ceec87d7
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-08 16:00:38 +0800

    Add `with_payment` scope

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -13,4 +13,22 @@ class Donation < ApplicationRecord
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 200 }
 
   delegate :organization, to: :fundraising_campaign
+
+  scope :with_payment, -> { joins(:payment).select(DONATION_FIELDS) }
+
+  DONATION_FIELDS = %w[
+    donations.id
+    donations.donation_id
+    donations.amount
+    donations.donor_id
+    donations.fundraising_campaign_id
+    payments.gross_amount
+    payments.fee
+    payments.net_amount
+    payments.payment_method
+    payments.status
+    payments.completed_at
+  ].freeze
+
+  private_constant :DONATION_FIELDS
 end
