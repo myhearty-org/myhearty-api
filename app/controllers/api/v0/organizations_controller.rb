25ca55b7ccad6bb5eba39b9012453b2e514c266c
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-09 02:59:00 +0800

    Move `update` action from `api::v0::organizations` to `organizations`

--- app/controllers/api/v0/organizations_controller.rb
+++ app/controllers/api/v0/organizations_controller.rb
@@ -3,49 +3,12 @@
 module Api
   module V0
     class OrganizationsController < ApiController
-      before_action :authenticate_organization_admin!, only: %i[update]
-
       def index
         @organizations = Organization.all
       end
 
       def show
         @organization = Organization.find(params[:id])
-      end
-
-      def update
-        @organization = Organization.find(params[:id])
-        result = Organizations::UpdateService.call(current_organization_admin, @organization, organization_params)
-
-        if result.success?
-          render :show, status: :ok
-        else
-          render_error(result.json, result.http_status)
-        end
-      end
-
-      private
-
-      def organization_params
-        params.require(:organization).permit(organization_params_attributes)
-      end
-
-      def organization_params_attributes
-        %i[
-          name
-          location
-          email
-          contact_no
-          website_url
-          facebook_url
-          youtube_url
-          person_in_charge_name
-          avatar
-          video_url
-          images
-          about_us
-          programmes_summary
-        ]
       end
     end
   end
