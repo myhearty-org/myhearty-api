d45639727a6af04ef311dba5825044075acaf8bb
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-09 19:59:37 +0800

    Move `donate` action from `campaigns` controller to `donations` controller

--- app/controllers/api/v0/fundraising_campaigns_controller.rb
+++ app/controllers/api/v0/fundraising_campaigns_controller.rb
@@ -4,7 +4,6 @@ module Api
   module V0
     class FundraisingCampaignsController < ApiController
       before_action :authenticate_member!, only: %i[create update destroy]
-      before_action :authenticate_user!, only: %i[donate]
 
       def index
         if params.key?(:organization_id)
@@ -50,21 +49,6 @@ module Api
         end
       end
 
-      def donate
-        fundraising_campaign = FundraisingCampaign.find(params[:id])
-
-        result = Donations::CreateService.call(current_user, fundraising_campaign, params[:amount])
-        donation = result.record
-
-        return render_error(result.json, result.http_status) unless result.success?
-
-        stripe_checkout_session = create_stripe_checkout_session(current_user, fundraising_campaign, donation)
-
-        Payments::CreateService.call(current_user, fundraising_campaign, donation, stripe_checkout_session)
-
-        redirect_to stripe_checkout_session.url, allow_other_host: true
-      end
-
       private
 
       def fundraising_campaign_params
@@ -83,26 +67,6 @@ module Api
           end_datetime
           published
         ]
-      end
-
-      def create_stripe_checkout_session(donor, fundraising_campaign, donation)
-        Stripe::Checkout::Session.create({
-          line_items: [{
-            price_data: {
-              currency: "myr",
-              unit_amount: donation.amount,
-              product: fundraising_campaign.fundraising_campaign_id
-            },
-            quantity: 1
-          }],
-          mode: "payment",
-          payment_method_types: ["alipay", "card", "fpx", "grabpay"],
-          submit_type: "donate",
-          customer_email: donor.email,
-          client_reference_id: fundraising_campaign.fundraising_campaign_id,
-          success_url: "https://myhearty.my/campaigns/#{fundraising_campaign.id}?session_id={CHECKOUT_SESSION_ID}",
-          cancel_url: "https://myhearty.my/campaigns/#{fundraising_campaign.id}?cancel=true"
-        }, { stripe_account: fundraising_campaign.organization.stripe_account_id })
       end
     end
   end
