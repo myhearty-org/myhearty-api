3fd4e99e0e1e222e69de6229e226c63fef7de649
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-09 16:30:12 +0800

    Add `volunteer_events` destroy service

---
+++ app/services/volunteer_events/destroy_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module VolunteerEvents
+  class DestroyService < BaseService
+    def initialize(member, id)
+      @member = member
+      @id = id
+    end
+
+    def call
+      volunteer_event = VolunteerEvent.find_by(id: id, organization: member.organization)
+
+      return error(http_status: :not_found) unless volunteer_event
+
+      return error_unallowed_delete if volunteer_event.published?
+
+      volunteer_event.delete
+      success
+    end
+
+    private
+
+    attr_reader :member, :id
+
+    def error_unallowed_delete
+      error(
+        json: { message: "Not allowed to delete published volunteer event" },
+        http_status: :forbidden
+      )
+    end
+  end
+end
