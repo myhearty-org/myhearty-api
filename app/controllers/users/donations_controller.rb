f4659b1945fc8dffab8ebfacaa2042d3b075ebc4
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-09 20:01:21 +0800

    Change users donations controller (#56)
    * User `users` `donations` controller to handle `donate` action
    
    * Move `donate` action from `campaigns` controller to `donations` controller

--- app/controllers/users/donations_controller.rb
+++ app/controllers/users/donations_controller.rb
@@ -7,5 +7,40 @@ module Users
     def index
       @donations = current_user.donations.with_payment
     end
+
+    def donate
+      fundraising_campaign = FundraisingCampaign.find(params[:id])
+      result = Donations::CreateService.call(current_user, fundraising_campaign, params[:amount])
+      donation = result.record
+
+      return render_error(result.json, result.http_status) unless result.success?
+
+      stripe_checkout_session = create_stripe_checkout_session(current_user, fundraising_campaign, donation)
+      Payments::CreateService.call(current_user, fundraising_campaign, donation, stripe_checkout_session)
+
+      redirect_to stripe_checkout_session.url, allow_other_host: true
+    end
+
+    private
+
+    def create_stripe_checkout_session(donor, fundraising_campaign, donation)
+      Stripe::Checkout::Session.create({
+        line_items: [{
+          price_data: {
+            currency: "myr",
+            unit_amount: donation.amount,
+            product: fundraising_campaign.fundraising_campaign_id
+          },
+          quantity: 1
+        }],
+        mode: "payment",
+        payment_method_types: ["alipay", "card", "fpx", "grabpay"],
+        submit_type: "donate",
+        customer_email: donor.email,
+        client_reference_id: fundraising_campaign.fundraising_campaign_id,
+        success_url: "https://myhearty.my/campaigns/#{fundraising_campaign.id}?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "https://myhearty.my/campaigns/#{fundraising_campaign.id}?cancel=true"
+      }, { stripe_account: fundraising_campaign.organization.stripe_account_id })
+    end
   end
 end
