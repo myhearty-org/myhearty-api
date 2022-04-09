57f7dd5703ec9c9c6f0182121e2f30c3ee4d073c
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-09 16:58:09 +0800

    Move controllers out of `api/v0` modules

---
+++ app/controllers/users/charitable_aid_applications_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Users
+  class CharitableAidApplicationsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      @charitable_aid_applications = current_user.charitable_aid_applications
+    end
+
+    def applied
+      charitable_aid_id = params[:charitable_aid_id]
+      charitable_aid_application = CharitableAidApplication.find_by(receiver: current_user, charitable_aid_id: charitable_aid_id)
+
+      charitable_aid_application ? head(:no_content) : head(:not_found)
+    end
+
+    def apply
+      charitable_aid = CharitableAid.find(params[:charitable_aid_id])
+      result = CharitableAidApplications::CreateService.call(current_user, charitable_aid)
+      @charitable_aid_application = result.record
+
+      if result.success?
+        render :show, status: :created
+      else
+        render_error(result.json, result.http_status)
+      end
+    end
+
+    def unapply
+      charitable_aid = CharitableAid.find(params[:charitable_aid_id])
+      result = CharitableAidApplications::DestroyService.call(current_user, charitable_aid)
+
+      if result.success?
+        head :no_content
+      else
+        render_error(result.json, result.http_status)
+      end
+    end
+  end
+end
