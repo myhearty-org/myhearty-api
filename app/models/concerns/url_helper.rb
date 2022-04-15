909e557c2fc4e378451c04af8130b078cc1cfa6e
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-15 21:16:22 +0800

    Add URL helper concern

---
+++ app/models/concerns/url_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module UrlHelper
+  extend ActiveSupport::Concern
+
+  included do
+    include Rails.application.routes.url_helpers
+
+    def self.default_url_options
+      Rails.application.config.action_controller.default_url_options
+    end
+  end
+
+  def url
+    url_for(self)
+  end
+end
