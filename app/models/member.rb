8547b969bd1aa9f076bb77eb2afb9aca2327451a
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-17 20:11:53 +0800

    Configure `user` and `member` to send Devise emails asynchronously

--- app/models/member.rb
+++ app/models/member.rb
@@ -16,4 +16,10 @@ class Member < ApplicationRecord
   delegate :charity?, to: :organization
 
   scope :admin, -> { where(admin: true) }
+
+  protected
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ApplicationRecord
          :rememberable, :validatable, :confirmable, :lockable,
          :timeoutable, :trackable, :omniauthable,
          omniauth_providers: Identity::PROVIDERS
+
+  protected
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
