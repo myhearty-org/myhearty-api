2cc9495b6ba7419bcf6c29a6a71cdfea72b440a3
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-04-19 02:34:03 +0800

    Remove `validates_timeliness` gem
    - Not customizable to produce proper error message

--- config/initializers/validates_timeliness.rb
+++
@@ -1,6 +0,0 @@
-ValidatesTimeliness.setup do |config|
-  # Extend ORM/ODMs for full support (:active_record).
-  config.extend_orms = [:active_record]
-
-  config.restriction_shorthand_symbols.update(time_current: lambda { Time.current })
-end
