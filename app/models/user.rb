f8b24b575e98d77100de7e30a44399f3374d478c
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-05-22 06:10:27 +0800

    Add user profile fields into `user` model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   include ImageUploader::Attachment(:avatar)
 
+  enum gender: %i[male female]
+
   has_many :identities, dependent: :delete_all
   has_many :donations, foreign_key: :donor_id
   has_many :payments
@@ -14,9 +16,21 @@ class User < ApplicationRecord
          :timeoutable, :trackable, :omniauthable,
          omniauth_providers: Identity::PROVIDERS
 
+  validates :name, allow_nil: true, length: { maximum: 63 }
+  validates :contact_no, phone: true, allow_nil: true, length: { maximum: 20 }
+  validates :address, allow_nil: true, length: { maximum: 255 }
+  validate :birth_date_must_be_before_current_date
+  validates :gender, allow_nil: true, inclusion: { in: genders.keys }
+
   protected
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  def birth_date_must_be_before_current_date
+    return if birth_date.blank?
+
+    errors.add(:birth_date, :must_be_before_current_date) if birth_date.to_i >= Time.current.to_i
   end
 end
